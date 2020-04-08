ce33a48548b9e889d30ce8f5b4fede0dee78cb77
peaBerberian <pea.berberian@gmail.com>
2020-04-08 18:48:53 +0200

    source-buffers: The queuedSourceBuffer is locked by default

--- src/core/source_buffers/queued_source_buffer.ts
+++ src/core/source_buffers/queued_source_buffer.ts
@@ -227,7 +227,7 @@ export default class QueuedSourceBuffer<T> {
     this.bufferType = bufferType;
     this._sourceBuffer = sourceBuffer;
     this._queue = [];
-    this._isLocked = false;
+    this._isLocked = true;
     this._pendingTask = null;
     this._lastInitSegment = null;
     this._currentCodec = codec;
